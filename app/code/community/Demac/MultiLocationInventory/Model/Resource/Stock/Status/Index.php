e971a25be311373db641afec92f5b89f11a845ee
Lee Saferite <lee.saferite@aoe.com>
2016-07-11 15:55:13 +0000

    Database specific helper code belongs in a resource model, not a generic helper

--- app/code/community/Demac/MultiLocationInventory/Model/Resource/Stock/Status/Index.php
+++ app/code/community/Demac/MultiLocationInventory/Model/Resource/Stock/Status/Index.php
@@ -20,8 +20,7 @@ class Demac_MultiLocationInventory_Model_Resource_Stock_Status_Index
      */
     public function updateCoreStockStatus($productIds = false)
     {
-        $query = Mage::helper('demac_multilocationinventory/indexer')->getUpdateCoreStockStatusQuery($productIds);
-        $this->_getWriteAdapter()->query($query);
+        $this->_getWriteAdapter()->query($this->getUpdateCoreStockStatusQuery($productIds));
     }
 
     /**
@@ -31,8 +30,7 @@ class Demac_MultiLocationInventory_Model_Resource_Stock_Status_Index
      */
     public function updateCoreStockItem($productIds = false)
     {
-        $coreStockItemUpdateQuery = Mage::helper('demac_multilocationinventory/indexer')->getUpdateCoreStockItemQuery($productIds);
-        $this->_getWriteAdapter()->query($coreStockItemUpdateQuery);
+        $this->_getWriteAdapter()->query($this->getUpdateCoreStockItemQuery($productIds));
     }
 
     /**
@@ -116,5 +114,85 @@ class Demac_MultiLocationInventory_Model_Resource_Stock_Status_Index
             ' GROUP BY CONCAT(store.store_id, \'_\', e.entity_id)'
             . ')';
         $this->_getWriteAdapter()->query($query);
+    }
+
+    /**
+     * Get Global Stock Status Select (for use in updates)
+     *
+     * @param bool  $productIds
+     * @param array $additionalFields
+     *
+     * @return string
+     */
+    protected function getGlobalStockStatusSelectQuery($productIds = false, $additionalFields = array())
+    {
+        $fields = array('product_id', 'qty', 'is_in_stock', 'manage_stock');
+        $fields = array_merge($fields, $additionalFields);
+
+        $stockStatusCollection = Mage::getModel('demac_multilocationinventory/stock_status_index')
+            ->getCollection();
+        $stockStatusCollection
+            ->addFieldToSelect($fields)
+            ->addFieldToFilter('store_id', 0);
+        if($productIds !== false) {
+            $stockStatusCollection
+                ->addFieldToFilter(
+                    'product_id',
+                    array(
+                        'in' => $productIds
+                    )
+                );
+        }
+
+        return $stockStatusCollection->getSelectSql(true);
+    }
+
+    /**
+     * Get core stock status update query.
+     *
+     * @param bool|array $productIds
+     *
+     * @return array|string
+     *
+     * @TODO Update to use varien query builder.
+     */
+    protected function getUpdateCoreStockStatusQuery($productIds = false)
+    {
+
+        $coreStockStatusTable  = Mage::getModel('core/resource')->getTableName('cataloginventory/stock_status');
+        $query                 =
+            'UPDATE'
+            . '  ' . $coreStockStatusTable . ' dest,'
+            . '  (' . $this->getGlobalStockStatusSelectQuery($productIds) . ') src '
+            . '  SET dest.qty = src.qty,'
+            . '    dest.stock_status = src.is_in_stock'
+            . '  WHERE dest.product_id = src.product_id';
+
+        return $query;
+    }
+
+    /**
+     * Get core stock item update query.
+     *
+     * @param bool|array $productIds
+     *
+     * @return array|string
+     *
+     * @TODO Update to use varien query builder.
+     */
+    protected function getUpdateCoreStockItemQuery($productIds = false)
+    {
+        $coreStockItemTable    = Mage::getModel('core/resource')->getTableName('cataloginventory/stock_item');
+        $query                 =
+            'UPDATE'
+            . '  ' . $coreStockItemTable . ' dest,'
+            . '  (' . $this->getGlobalStockStatusSelectQuery($productIds, array('backorders')) . ') src '
+            . '  SET dest.qty = src.qty,'
+            . '    dest.is_in_stock = src.is_in_stock,'
+            . '    dest.backorders = src.backorders,'
+            . '    dest.manage_stock = src.manage_stock'
+            . '  WHERE dest.product_id = src.product_id';
+
+        return $query;
     }
 }
